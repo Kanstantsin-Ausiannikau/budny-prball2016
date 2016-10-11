beabae646efc75e9c79130276c0c0817a0c5d36f
Kanstantsin Ausiannikau <2000@tut.by>
2016-10-11 17:58:59 +0300

    New CFData Class - for new custom field group

---
+++ prBall/CFData2016.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prBall
+{
+    class CFData2016
+    {
+        public string Title { get; set; }
+        public int ArticleID { get; set; }
+        public int GorodID { get; set; }
+        public int VuzID { get; set; }
+        public int FakultetID { get; set; }
+
+        public bool? TypeObuchDnevnoe { get; set; }
+        public bool? TypeObuchZaochnoe { get; set; }
+        public bool? TypeObuchDistanc { get; set; }
+        public bool? TypeObuchSokrasch { get; set; }
+
+        public decimal? PrBallDnevnBudget { get; set; }
+        public decimal? PrBallDnevnPlatnoe { get; set; }
+        public decimal? PrBallZaochnBudget { get; set; }
+        public decimal? PrBallZaochnPlatn { get; set; }
+        public decimal? PrBallSokrDnevnBudg { get; set; }
+        public decimal? PrBallSokrDnevnPlatn { get; set; }
+        public decimal? PrBallSokrZaochBudget { get; set; }
+        public decimal? PrBallSokrZaochPlatnoe { get; set; }
+        public decimal? PrBallDistBudget { get; set; }
+        public decimal? PrBallDistPlatnoe { get; set; }
+
+        public bool? CertRusskiy { get; set; }
+        public bool? CertMatemat { get; set; }
+        public bool? CertHimia { get; set; }
+        public bool? CertIstoriaBelorus { get; set; }
+        public bool? CertInostrYazik { get; set; }
+        public bool? CertSpecEkzamen { get; set; }
+        public bool? CertBiologia { get; set; }
+
+        public bool? CertFizika { get; set; }
+        public bool? CertObschestvoved { get; set; }
+        public bool? CertVsemirIstoria { get; set; }
+        public bool? CertGeografia { get; set; }
+        public bool? CertProfSobesed { get; set; }
+
+        public int NapravleniePodgotovki { get; set; }
+    }
+}
