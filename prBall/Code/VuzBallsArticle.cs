9d2b16f84f7320e0eb6e7bc48ec127995e55bf18
Kanstantsin Ausiannikau <2000@tut.by>
2017-10-31 22:05:27 +0300

    Add VuzBallsArticle.cs

---
+++ prBall/Code/VuzBallsArticle.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prBall.Code
+{
+    public class VuzBallsArticle
+    {
+
+        static List<Article> _articles;
+
+        StringBuilder _html;
+
+        public void AddBallsCategory(string category)
+        {
+            _html.Append(category.H2());
+        }
+        public VuzBallsArticle()
+        {
+            _html = new StringBuilder();
+        }
+
+        public void AddSection(StringTable st, string name)
+        {
+            _html.Append(name.P().Strong());
+            _html.Append(HtmlHelper.GetAdaptiveDesignTable(st));
+        }
+
+        public string GetHtml()
+        {
+            return _html.ToString();
+        }
+
+        public static StringTable GetStringTableFromCFData(List<CFData2016> data, TypeLearning type)
+        {
+            StringTable st = new StringTable(data.Count + 1, 2);
+            st.AddHeader("Специальности (направления специальностей)", "Проходной балл");
+
+            List<Article> articles = GetAllSpecialityArticles();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                var currentArticle = articles.Find(a => a.SubTitle.Contains(data[i].Title.Substring(0, 10)));
+
+                string link = String.Format("/abiturient/specialnosty/artmid/520/articleid/{0}/{1}", currentArticle.ArticleID, currentArticle.TitleLink).ToLower();
+
+                string aTeg = data[i].Title.A(link);
+
+                switch (type)
+                {
+                    case TypeLearning.DnevnoeBudg:
+                        st.AddRow(aTeg, data[i].PrBallDnevnBudget.ToString());
+                        break;
+                    case TypeLearning.DnevnoePlatn:
+                        st.AddRow(aTeg, data[i].PrBallDnevnPlatnoe.ToString());
+                        break;
+                    case TypeLearning.ZaochnoeBudg:
+                        st.AddRow(aTeg, data[i].PrBallZaochnBudget.ToString());
+                        break;
+                    case TypeLearning.ZaochnoePlatnoe:
+                        st.AddRow(aTeg, data[i].PrBallZaochnPlatn.ToString());
+                        break;
+                    case TypeLearning.DistancionnoeBudg:
+                        st.AddRow(aTeg, data[i].PrBallDistBudget.ToString());
+                        break;
+                    case TypeLearning.DistancionnoePlatn:
+                        st.AddRow(aTeg, data[i].PrBallDistPlatnoe.ToString());
+                        break;
+                    case TypeLearning.SokraschennoeBudg:
+                        st.AddRow(aTeg, data[i].PrBallSokrDnevnBudg.ToString());
+                        break;
+                    case TypeLearning.SokraschennoePlatn:
+                        st.AddRow(aTeg, data[i].PrBallSokrDnevnPlatn.ToString());
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return st;
+        }
+
+        private static List<Article> GetAllSpecialityArticles()
+        {
+            if (_articles==null)
+            {
+                _articles = new List<Article>();
+            }
+            else
+            {
+                return _articles;
+            }
+
+            var specArticles = Data.GetArticlesIDFromCategoryID(29);
+
+            foreach (int id in specArticles)
+            {
+                _articles.Add(Data.GetArticleFromID(id));
+            }
+
+            return _articles;
+        }
+
+        internal void AddPTag(string txt="")
+        {
+            _html.Append(txt.P());
+        }
+    }
+}
